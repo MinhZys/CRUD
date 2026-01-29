d71c498f406284c7af465a518aeae759dc67bd0a
tmquya23088 <tmquya23088@cusc.ctu.edu.vn>
2026-01-30 01:14:18 +0700

    Add ASP.NET CRUD project

--- CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5617a623a2bdff6a4ac1258ec90bad2c752b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
